c6533f86a17232531827bf58a52509a569a767ce
Ben Winter <benjamin.a.winter@gmail.com>
2020-03-23 23:58:25 -0500

    update

--- item.php
+++ item.php
@@ -67,7 +67,7 @@ if ( isset( $favorite ) ) {
 }
 
 $help = "<p>You are looking at a list of ".$_SESSION['SELECTOR'];
-if ( $_SESSION['SELECTOR'] == "domestic pets" ) {
+if ( $_SESSION['SELECTOR'] == "domesticated pets" ) {
   $help = $help." from amongst my work colleagues.";
 } else {
   $help = $help. ".";
